e93a3a3624a776780f2a6961ac9e4cf2b7f2407c
幻缘 <578267200@qq.com>
2018-08-20 00:00:51 +0800

    添加启动成功提示

--- bys-web/src/main/java/com/chong/bys/WebApplication.java
+++ bys-web/src/main/java/com/chong/bys/WebApplication.java
@@ -1,5 +1,6 @@
 package com.chong.bys;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  * @date 2018/08/19 22:19
  * @since 1.0
  */
+@Slf4j
 @EnableCaching //开启缓存注解模式
 @EnableRedisHttpSession //开启redisSession
 @EnableRabbit //开启rabbitmq注解模式
@@ -19,5 +21,6 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+		log.info("bysApplication is success!");
 	}
 }
