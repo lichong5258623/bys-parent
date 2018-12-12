a9180bf01ec01d234306f8e926ea264a7d6553b9
lichong <578267200@qq.com>
2018-12-13 00:35:42 +0800

    添加dubbo服务（文章）---提交遗漏

---
+++ bys-artical-service/src/test/java/com/chong/bys/articalservice/BysArticalServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chong.bys.articalservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BysArticalServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
