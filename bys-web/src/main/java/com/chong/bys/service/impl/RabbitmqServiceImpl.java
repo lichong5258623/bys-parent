db4251032cbadef219db0787deccb4326431cb16
幻缘 <578267200@qq.com>
2019-01-28 17:52:40 +0800

    添加test模块

---
+++ bys-web/src/main/java/com/chong/bys/service/impl/RabbitmqServiceImpl.java
@@ -0,0 +1,22 @@
+/**************************************************************************
+ * Copyright (c) 2018-2022 ZheJiang Electronic Port, Inc.
+ * All rights reserved.
+ *
+ * 项目名称：海蛛
+ * 版权说明：本软件属浙江电子口岸有限公司所有，在未获得浙江电子口岸有限公司正式授权
+ *           情况下，任何企业和个人，不能获取、阅读、安装、传播本软件涉及的任何受知
+ *           识产权保护的内容。                            
+ ***************************************************************************/
+package com.chong.bys.service.impl;
+
+import com.chong.bys.service.RabbitmqService;
+
+/**
+ * 功能说明：
+ *
+ * @author : <a href="mailto:lichong@zjport.gov.cn">lichong</a>
+ * @version : 1.0
+ * @since 3.0
+ */
+public class RabbitmqServiceImpl implements RabbitmqService {
+}
