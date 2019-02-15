591d9f8a9361033a3307060f4ebc004468f611b0
幻缘 <578267200@qq.com>
2019-02-15 17:24:17 +0800

    添加countdownluch使用例子

---
+++ bys-web/src/main/java/com/chong/bys/initask/StartupSuccessRunner02.java
@@ -0,0 +1,28 @@
+package com.chong.bys.initask;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * 功能说明：项目启动完毕后执行的方法，order注解可以设置多个的启动顺序
+ *
+ * @author : <a href="mailto:lichong@zjport.gov.cn">lichong</a>
+ * @version : 1.0
+ * @since 3.0
+ */
+@Component
+@Order(value = 2)
+@Slf4j
+public class StartupSuccessRunner02 implements CommandLineRunner {
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming main method arguments
+     */
+    @Override
+    public void run(String... args) {
+        log.info("项目启动完毕后执行的方法二。。。。");
+    }
+}
