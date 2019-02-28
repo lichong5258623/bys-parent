95db4870913fd82b736f5d6bf19b4bd6caf382bb
幻缘 <578267200@qq.com>
2019-02-28 17:12:45 +0800

    日志完善

--- bys-artical-service/src/main/java/com/chong/bys/BysArticalServiceApplication.java
+++ bys-artical-service/src/main/java/com/chong/bys/BysArticalServiceApplication.java
@@ -1,14 +1,17 @@
 package com.chong.bys;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @EnableDubbo
 @SpringBootApplication
 public class BysArticalServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BysArticalServiceApplication.class, args);
+        log.info("文章服务启动完毕......");
     }
 }
