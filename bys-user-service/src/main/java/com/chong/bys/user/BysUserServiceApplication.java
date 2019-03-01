85ba50222aabbdb2e5e64dc592c88cea6521627d
幻缘 <578267200@qq.com>
2019-03-01 11:53:19 +0800

    优化日志

--- bys-user-service/src/main/java/com/chong/bys/user/BysUserServiceApplication.java
+++ bys-user-service/src/main/java/com/chong/bys/user/BysUserServiceApplication.java
@@ -1,10 +1,12 @@
 package com.chong.bys.user;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @EnableDubbo
 @SpringBootApplication
 @MapperScan("com.chong.bys.user.dao")
@@ -12,5 +14,6 @@ public class BysUserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BysUserServiceApplication.class, args);
+        log.info("用户服务启动完毕");
     }
 }
