3cfec8a6a0b04269ef795232993e89f894d6b3b1
lichong <578267200@qq.com>
2019-03-15 00:22:49 +0800

    代码优化

--- bys-artical-service/src/main/java/com/chong/bys/BysArticalServiceApplication.java
+++ bys-artical-service/src/main/java/com/chong/bys/BysArticalServiceApplication.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lichong
+ */
 @Slf4j
 @EnableDubbo
 @SpringBootApplication
